6fddbdda1e7d07898be75b689668bbbbdb04406c
Ewa Sek <ewasek@gmail.com>
2017-06-17 19:31:41 +0200

    todolist

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -193,7 +193,7 @@ class AdminController extends Controller
     public function listAction()
     {
         $tasksRepository = $this->getDoctrine()->getRepository('AppBundle:ToDoList');
-        $tasks = $tasksRepository->findAll();
+        $tasks = $tasksRepository->findBy(array(), array('isDone' => 'ASC','deadline' => 'ASC'));
 
         return ['tasks' => $tasks];
     }
@@ -210,19 +210,60 @@ class AdminController extends Controller
         $todo = $request->request->get('task');
         $deadline = $request->request->get('deadline');
 
-        $task = new ToDo();
+        $task = new ToDoList();
         $task->setTask($todo);
+
         if ($deadline != "") {
             $task->setDeadline(new \DateTime($deadline));
+        } else {
+            $task->setDeadline(new \DateTime('now'));
         }
+
         $task->setIsDone(0);
-        $task->setUser($this->getUser());
 
         $em->persist($task);
         $em->flush();
 
         $tasksRepository = $this->getDoctrine()->getRepository('AppBundle:ToDoList');
-        $tasks = $tasksRepository->findAll();
+        $tasks = $tasksRepository->findBy(array(), array('isDone' => 'ASC','deadline' => 'ASC'));
+
+        return ['tasks' => $tasks];
+    }
+
+    /**
+     * @Route("/list/done/{id}", name="done_list")
+     * @Template("AppBundle:Admin:list.html.twig")
+     */
+    public function doneListAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $tasksRepository = $this->getDoctrine()->getRepository('AppBundle:ToDoList');
+        $task = $tasksRepository->find($id);
+        $task->setIsDone(1);
+        $em->persist($task);
+        $em->flush();
+
+        $tasksRepository = $this->getDoctrine()->getRepository('AppBundle:ToDoList');
+        $tasks = $tasksRepository->findBy(array(), array('isDone' => 'ASC','deadline' => 'ASC'));
+
+        return ['tasks' => $tasks];
+
+    }
+
+    /**
+     * @Route("/list/delete/{id}", name="delete_list")
+     * @Template("AppBundle:Admin:list.html.twig")
+     */
+    public function deleteListAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $tasksRepository = $this->getDoctrine()->getRepository('AppBundle:ToDoList');
+        $task = $tasksRepository->find($id);
+        $em->remove($task);
+        $em->flush();
+
+        $tasksRepository = $this->getDoctrine()->getRepository('AppBundle:ToDoList');
+        $tasks = $tasksRepository->findBy(array(), array('isDone' => 'ASC','deadline' => 'ASC'));
 
         return ['tasks' => $tasks];
     }
